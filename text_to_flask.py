571527f703870a59673c4c98c5e43bbfff729ffd
ry-la <ryla@ucsd.edu>
2023-06-17 21:50:42 -0700

    Uploaded files via git

---
+++ text_to_flask.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('text_to_flask.html')
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    file = request.files['file']
+    # Save the file or process it as needed
+    print(file.name)
+    file.save(file.filename)
+    return 'File uploaded successfully.'
+
+if __name__ == '__main__':
+    app.run()
