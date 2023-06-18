6924f671c2bd3db5af06a8e2fdd016a1e83a809a
Mihir Baviskar <36090347+mihirbaviskar@users.noreply.github.com>
2023-06-18 02:21:14 -0700

    added quiz.html and quiz appearance

--- text_to_flask.py
+++ text_to_flask.py
@@ -2,7 +2,7 @@ import os
 
 from flask import Flask, render_template, request
 
-os.environ["OPENAI_API_KEY"] = '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'
+os.environ["OPENAI_API_KEY"] = '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'
 
 from llama_index import (  # LLMPredictor,; ServiceContext
     SimpleDirectoryReader, VectorStoreIndex)
@@ -13,7 +13,7 @@ app = Flask(__name__)
 def index():
     return render_template('text_to_flask.html')
 
-@app.route('/upload', methods=['POST'])
+@app.route('/quiz', methods=['POST'])
 def upload():
     file = request.files['file']
     # Save the file or process it as needed
@@ -30,24 +30,32 @@ def upload():
 
     query_engine = index.as_query_engine()
 
-    question = "Give a 4-choice multiple choice question without the answer about this topic"
+    question = "Give a 4-choice multiple choice question without the answer about this topic in the following format: Question,option1,option2,option3,option4"
 
     prompt = question
 
     response = query_engine.query(prompt)
-
+    response_list = str(response).split(",")
+    print(response_list)
     # history = history + \
     #        "They asked " + question + "\n" + \
     #        "You responded with " + str(response) + "\n"
 
-    print(response.response_txt)
+    print(str(response))
     print()
 
     print("history starting")
     #print(history)
     print()
 
-    return response.response_txt
+    return render_template(
+        "quiz.html",
+        question=response_list[0],
+        option1=response_list[1],
+        option2=response_list[2],
+        option3=response_list[3],
+        option4=response_list[4]
+    )
 
 if __name__ == '__main__':
     app.run()
